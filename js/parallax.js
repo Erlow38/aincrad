0d87c12941a044109aa91c75d47203d64e764a89
Erlow <erlowgaming@gmail.com>
2023-10-16 12:12:50 +0200

    first commit

---
+++ js/parallax.js
@@ -0,0 +1,17 @@
+// Get a reference to the video container and the hero container
+const videoContainer = document.querySelector('.background');
+const heroContainer = document.querySelector('.hero-container');
+
+// Define the parallax effect function
+function parallax() {
+    // Calculate the scroll position
+    const scrollPosition = window.scrollY;
+
+    // Apply the parallax effect to the video container
+    videoContainer.style.transform = `translateY(-${scrollPosition * 0.5}px)`;
+
+    // You can adjust the factor (0.5 in this example) to control the parallax speed
+}
+
+// Add an event listener to the window to call the parallax function on scroll
+window.addEventListener('scroll', parallax);
