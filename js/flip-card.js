569c6ffcd7ae515935b99e7255239dbcd4666e86
Ethan EHRLER <ethan.ehrler@etu.univ-grenoble-alpes.fr>
2023-10-21 00:34:39 +0200

    description card

--- js/flip-card.js
+++ js/flip-card.js
@@ -1,5 +1,6 @@
 const characterCard = document.querySelectorAll('.character-card');
 const levelCard = document.querySelectorAll('.level-card');
+const descriptionCard = document.querySelectorAll('.description-card');
 
 
 for (let i = 0; i < characterCard.length; i++) {
@@ -32,6 +33,23 @@ window.addEventListener('click', function(e) {
     if (!e.target.closest('.level-card')) {
         for (let i = 0; i < levelCard.length; i++) {
             levelCard[i].classList.remove('flipped');
+        }
+    }
+});
+
+for (let i = 0; i < descriptionCard.length; i++) {
+    descriptionCard[i].addEventListener('click', function() {
+        for (let j = 0; j < descriptionCard.length; j++) {
+            descriptionCard[j].classList.remove('flipped');
+        }
+        descriptionCard[i].classList.add('flipped');
+    });
+}
+
+window.addEventListener('click', function(e) {
+    if (!e.target.closest('.description-card')) {
+        for (let i = 0; i < descriptionCard.length; i++) {
+            descriptionCard[i].classList.remove('flipped');
         }
     }
 });
